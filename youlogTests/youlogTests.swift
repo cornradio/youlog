5f1d77eafd792051a422978383908150a15fbcd8
Name <kasusaland@gmail.com>
2025-03-28 21:44:22 +0800

    Initial Commit

---
+++ youlogTests/youlogTests.swift
@@ -0,0 +1,17 @@
+//
+//  youlogTests.swift
+//  youlogTests
+//
+//  Created by kasusa on 2025/3/28.
+//
+
+import Testing
+@testable import youlog
+
+struct youlogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
